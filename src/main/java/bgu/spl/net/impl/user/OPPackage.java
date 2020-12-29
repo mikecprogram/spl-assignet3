f0c31bab2b696868aca35266ddc8113351685033
faiferpost <faifer@post.bgu.ac.il>
2020-12-29 02:18:54 +0200

    OPPackage - added OP codes declaration

--- src/main/java/bgu/spl/net/impl/user/OPPackage.java
+++ src/main/java/bgu/spl/net/impl/user/OPPackage.java
@@ -1,6 +1,22 @@
 package bgu.spl.net.impl.user;
 
 public class OPPackage{
+    //These codes are here because they are being used only with OPPackage.
+    //Therefore they don't have to be static.
+    public final int OP_ADMIN_REGISTER = 1;
+    public final int OP_STUDENT_REGISTER = 2;
+    public final int OP_LOGIN_REQUEST = 3;
+    public final int OP_LOGOUT_REQUEST = 4;
+    public final int OP_REGISTER_COURSE = 5;
+    public final int OP_CHECK_KDAM = 6;
+    public final int OP_A_COURSE_STAT = 7;
+    public final int OP_A_STUDENT_STAT = 8;
+    public final int OP_CHECK_REGISTERED = 9;
+    public final int OP_UNREGISTER_COURSE = 10;
+    public final int OP_CHECK_MY_COURSES = 11;
+    public final int OP_ACK = 12;
+    public final int OP_ERROR = 13;
+
     private int OPCode;
     private String firstArg;
     private String secondArg;
