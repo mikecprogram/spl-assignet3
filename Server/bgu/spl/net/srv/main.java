c706fbbd3cc3ad1ef843cd5d18de3fe63bdc6cc8
faiferpost <faifer@post.bgu.ac.il>
2021-01-01 18:28:36 +0200

    Just a simple pull and push !!!(!)

--- Server/bgu/spl/net/srv/main.java
+++ Server/bgu/spl/net/srv/main.java
@@ -8,7 +8,7 @@ import bgu.spl.net.impl.user.UserProtocol;
 public class main {
     public static void main(String[] args) {
         if(!Database.getInstance().initialize("./Courses.txt")) {
-            Reactor<OPPackage> server = new Reactor<OPPackage>(Integer.decode(args[1]), Integer.decode(args[0]),
+            Reactor<OPPackage> server = new Reactor<>(Integer.decode(args[1]), Integer.decode(args[0]),
                     () -> new UserProtocol(),
                     () -> new OPPackageMessageEncoderDecoder());
             server.serve();
